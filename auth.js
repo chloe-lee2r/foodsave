36d0f17abbe5fe8e52e26779fcbbab5e9db3e3e0
Reuben <reubenyee2012@gmail.com>
2026-03-02 17:01:37 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,1 @@
-function requireAuth(requiredRole) {
-    const user = Parse.User.current();
 
-    if (!user) {
-        location.href = "login.html";
-        return;
-    }
-
-    const role = user.get("role");
-
-    if (requiredRole && role !== requiredRole) {
-        alert("Access denied");
-        location.href = "login.html";
-    }
-}
